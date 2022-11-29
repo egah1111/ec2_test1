fe20f9aeac79b3cf1a71f504e2a8e0ada88fa008
Egah Francis <108225524+egah1111@users.noreply.github.com>
2022-11-29 09:52:55 +0100

    go now

---
+++ utils/train_model.py
@@ -0,0 +1,31 @@
+"""
+    Simple file to create a sklearn model for deployment in our API
+
+    Author: Explore Data Science Academy
+
+    Description: This script is responsible for training a simple linear
+    regression model which is used within the API for initial demonstration
+    purposes.
+
+"""
+
+# Dependencies
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+
+# Fetch training data and preprocess for modeling
+train = pd.read_csv('./data/df_train.csv')
+
+y_train = train[['load_shortfall_3h']]
+X_train = train[['Madrid_wind_speed','Barcelona_temp_min','Bilbao_pressure', "Barcelona_pressure"]]
+
+# Fit model
+lm_regression = LinearRegression(normalize=True)
+print ("Training Model...")
+lm_regression.fit(X_train, y_train)
+
+# Pickle model for use within our API
+save_path = '../assets/trained-models/load_shortfall_simple_lm_regression_team1.pkl'
+print (f"Training completed. Saving model to: {save_path}")
+pickle.dump(lm_regression, open(save_path,'wb'))
